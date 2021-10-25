b6f3f39a5803a06b097e93dc26315b5a7aae6659
farkhod <986532qw@gmail.com>
2021-10-25 20:52:33 +0300

    Initial Commit

---
+++ BindingPropertyDemo/BindingPropertyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingPropertyDemoApp.swift
+//  BindingPropertyDemo
+//
+//  Created by Farkhod on 25.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct BindingPropertyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
